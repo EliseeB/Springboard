1412f93c4ce243559aa090bf0822ea9024c59676
EliseeB <eliseebatumarai@gmail.com>
2023-07-31 18:28:04 -0700

    mg

--- Meme_Generator/main.js
+++ Meme_Generator/main.js
@@ -1,42 +1,41 @@
-
+// Get the form and container elements from the HTML
 const form = document.querySelector('.form')
-
 const container = document.querySelector('.Container')
 
-
-
+// Add a submit event listener to the form to handle meme generation
 form.addEventListener('submit', event => {
     event.preventDefault()
+
+    // Get input values from the form
     const urlImageInput = document.querySelector('#urlImage')
-
     const InputForTopText = document.querySelector('#topText')
-
     const inputForBottomText = document.querySelector('#bottomText')
 
+    // Create an image element to display the meme
     const image = document.createElement('img')
     image.classList.add('meme')
     const srcImage = urlImageInput.value
     image.setAttribute('src', `${srcImage}`)
 
-
-    let  memeTop = document.createElement('h3')
-        memeTop.classList.add('topt-ext')
-
-
+    // Create h3 elements for the top and bottom text of the meme
+    let memeTop = document.createElement('h3')
+    memeTop.classList.add('topt-ext')
     const topTextValue = InputForTopText.value
     memeTop.innerText = topTextValue
-    
+
     const memeBottom = document.createElement('h3')
     memeBottom.classList.add('bottom-text')
     const bottomTextValue = inputForBottomText.value
     memeBottom.innerText = bottomTextValue
 
+    // Create a button element to delete the meme
     const deleteButton = document.createElement('button')
     deleteButton.classList.add('delete')
     deleteButton.innerText = 'Delete'
 
-
-    if (srcImage) {
+    // Check if an image URL is provided
+    if (srcImage && topTextValue && bottomTextValue) {
+        // Create a div container for the meme and append the meme elements to it
         const div = document.createElement('div')
         div.classList.add('memeContainer')
         div.appendChild(memeTop)
@@ -44,19 +43,21 @@ form.addEventListener('submit', event => {
         div.appendChild(image)
         div.appendChild(deleteButton)
         container.appendChild(div)
-       
+        form.reset()     // Reset the form after adding the meme 
     } else {
-        alert('Please add an Url')
+        // If no image URL, top Text, or bottom text is provided, show an alert message
+        alert('Missing text from one or more input field(S)')
     }
 })
 
+// Add a click event listener to the container to handle meme deletion and form reset
 container.addEventListener('click', event => {
     const target = event.target
 
-    if(target.className === 'delete') {
+    // Check if the clicked element is the delete button
+    if (target.className === 'delete') {
+        // Remove the meme container when the delete button is clicked
         target.parentElement.remove()
     }
-    
-    form.reset()
-
+  
 })
